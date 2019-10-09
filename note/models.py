9d3b027caa0a2ce1f6532cd6f5004ce4cb256a85
LeanneShen <maganda.shen@163.com>
2019-10-09 11:53:56 +0800

    無法顯示NOTE內文

--- note/models.py
+++ note/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 
 class Note(models.Model):
+    seq = models.IntegerField(default=0)
     subject = models.CharField(max_length=200)
+    detail = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.subject
-
-
--- note/views.py
+++ note/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Note
+def index(request):
+    list = Note.objects.all()
+    context = {'list': list}
+    return render(request, 'note/index.html', context)
